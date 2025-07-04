200b0aabd48c0481e36407fba52797e416eddeee
vash1945 <alhabib1945@gmail.com>
2025-07-05 02:11:41 +0700

    init commit: ShalatPlan PWA

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Tajawal', 'sans-serif'],
+            },
+            colors: {
+                'teal': {
+                    '50': '#f0fdfa',
+                    '100': '#ccfbf1',
+                    '200': '#99f6e4',
+                    '300': '#5eead4',
+                    '400': '#2dd4bf',
+                    '500': '#14b8a6',
+                    '600': '#0d9488',
+                    '700': '#0f766e',
+                    '800': '#115e59',
+                    '900': '#134e4a',
+                    '950': '#042f2e',
+                },
+            }
+        },
+    },
+    plugins: [],
+}
